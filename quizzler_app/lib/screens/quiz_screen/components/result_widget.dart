a809b4e9a9d6d33a68e9eaa2709be5bda1d7ca7a
Peter Sunde <redes_sociais_1900.kgwa5@simplelogin.co>
2022-05-02 21:21:26 -0300

    Refatorei o código e fiz com que agora o usuário tenha feedback se ele acertou ou não uma pergunta e também, caso ele chegue ao final do quiz não ocorra nenhum erro e o Widget de resultado apareça

---
+++ quizzler_app/lib/screens/quiz_screen/components/result_widget.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+class ResultWidget extends StatelessWidget {
+  const ResultWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: double.infinity,
+      width: double.infinity,
+      child: Column(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.05,
+          ),
+          Text(
+            "Congratulations!",
+            style: GoogleFonts.poppins(
+              color: const Color(0xFF313131),
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 40.0),
+            child: Text(
+              "You have answered 7 questions correctly",
+              style: GoogleFonts.poppins(
+                color: const Color(0xFF313131),
+                fontSize: 20,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.2,
+          ),
+          const ResultButton(
+            color: Color(0xFFFFC04A),
+            text: "Go back to home screen",
+          ),
+          const ResultButton(
+            color: Color(0xFFFFF9EF),
+            text: "Go back to home screen",
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ResultButton extends StatelessWidget {
+  const ResultButton({
+    required this.color,
+    required this.text,
+    Key? key,
+  }) : super(key: key);
+
+  final String text;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 40.0,
+        vertical: 20,
+      ),
+      child: SizedBox(
+        height: MediaQuery.of(context).size.height * 0.1,
+        child: ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            primary: color,
+            onPrimary: Colors.black,
+            elevation: 0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+          ),
+          child: Center(
+            child: Text(
+              text,
+              style: GoogleFonts.poppins(
+                fontSize: 20,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- quizzler_app/lib/screens/quiz_screen/quiz_screen.dart
+++ quizzler_app/lib/screens/quiz_screen/quiz_screen.dart
@@ -1,14 +1,22 @@
+import 'package:bootstrap_icons/bootstrap_icons.dart';
+import 'package:html_unescape/html_unescape.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:quizzler_app/screens/quiz_screen/components/answer_btn.dart';
+import 'package:quizzler_app/screens/quiz_screen/components/feedback_circle.dart';
 import 'package:quizzler_app/services/question_brain.dart';
 
 import '../../models/questions.dart';
+import 'components/result_widget.dart';
+//TODO: Dar feedback quando ele terminar
+//TODO: Dar feedback quando ele terminar sair
+
+//TODO: Quando o usuário clicar em um Container ele deve conseguir acessar o quiz correspondente a ela
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({Key? key, required this.questions}) : super(key: key);
 
-  final dynamic questions;
+  final List<Question> questions;
 
   @override
   State<QuestionScreen> createState() => _QuestionScreenState();
@@ -20,49 +28,86 @@ class _QuestionScreenState extends State<QuestionScreen> {
     super.initState();
   }
 
+  HtmlUnescape unescape = HtmlUnescape();
   int questionNumber = 1;
   int currentIndex = 0;
+  List<Widget> feedbackCircles = [];
   @override
   Widget build(BuildContext context) {
     Question currentQuestion = widget.questions[currentIndex];
     List<dynamic> allTheAnswers = QuizHelper.getAllTheAnswers(currentQuestion);
+
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            Expanded(
-              flex: 2,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 25.0,
-                  vertical: 20,
-                ),
-                child: Text(
-                  "$questionNumber. ${currentQuestion.question}",
-                  style: GoogleFonts.poppins(
-                    color: const Color(0xFF313131),
-                    fontWeight: FontWeight.bold,
-                    fontSize: 22,
+        child: currentIndex < widget.questions.length - 1
+            ? Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 25.0,
+                        vertical: 20,
+                      ),
+                      child: Column(
+                        children: [
+                          Text(
+                            "$questionNumber. ${unescape.convert(currentQuestion.question)}",
+                            style: GoogleFonts.poppins(
+                              color: const Color(0xFF313131),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 22,
+                            ),
+                          ),
+                          Flexible(
+                            child: Row(
+                              children: [
+                                ...feedbackCircles,
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
                   ),
-                ),
-              ),
-            ),
-            ...allTheAnswers.map((answer) {
-              return Expanded(
-                child: AnswerBtn(
-                  answerText: answer,
-                  onTap: () {
-                    setState(() {
-                      currentIndex++;
-                      questionNumber++;
-                    });
-                  },
-                ),
-              );
-            }).toList(),
-          ],
-        ),
+                  ...allTheAnswers.map((answer) {
+                    return Expanded(
+                      child: AnswerBtn(
+                        answerText: unescape.convert(answer),
+                        onTap: () {
+                          if (currentIndex < widget.questions.length - 1) {
+                            setState(
+                              () {
+                                currentIndex++;
+                                questionNumber++;
+                                {
+                                  if (answer == currentQuestion.correctAnswer) {
+                                    feedbackCircles.add(
+                                      const FeedbackCircle(
+                                        color: Color(0xFF41D664),
+                                        icon: BootstrapIcons.check,
+                                      ),
+                                    );
+                                  } else {
+                                    feedbackCircles.add(
+                                      const FeedbackCircle(
+                                        color: Color(0xFFFF5978),
+                                        icon: BootstrapIcons.x,
+                                      ),
+                                    );
+                                  }
+                                }
+                              },
+                            );
+                          }
+                        },
+                      ),
+                    );
+                  }).toList(),
+                ],
+              )
+            : const ResultWidget(),
       ),
     );
   }
